6a1f2848f8e78d22538a796c127f84d63a9f18be
KakwiriStephen <ngottakakwiri@gmail.com>
2024-11-19 12:18:04 +0300

    Add project files.

---
+++ KEWalks.Api/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KEWalks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult GetAllStudents()
+        {
+
+            string[] studentNames = new string[] { "Hadija", "Halima", "Amina", "Mwanaisha"};
+            return Ok(studentNames);
+        }
+    }
+}
