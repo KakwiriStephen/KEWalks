1818a1cd918ee502e6a3eb4aa21daf4859286fa0
KakwiriStephen <ngottakakwiri@gmail.com>
2024-11-26 07:14:21 +0300

    DBsets

---
+++ KEWalks.Api/Data/KEWalksDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KEWalks.Api.Data
+{
+    public class KEWalksDbContext : DbContext
+    {
+        public KEWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+    }
+}
---
+++ KEWalks.Api/Models/Domain/Difficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace KEWalks.Api.Models.Domain
+{
+    public class Difficulty
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
